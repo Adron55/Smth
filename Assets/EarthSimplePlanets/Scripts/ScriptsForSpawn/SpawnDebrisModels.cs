e752600029efd31e0c42ccda1c3cc6ff31f2b625
fred <fredfrede414@gmail.com>
2019-10-18 23:00:19 +0400

    added spaceshift view

---
+++ Assets/EarthSimplePlanets/Scripts/ScriptsForSpawn/SpawnDebrisModels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDebrisModels : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+    public float respawnTime = 1.0f;
+    private Vector2 screenBounds;
+
+    // Use this for initialization
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(asteroidWave());
+    }
+    private void spawnEnemy()
+    {
+        GameObject a = Instantiate(asteroidPrefab) as GameObject;
+        a.transform.position = new Vector3(screenBounds.x * -2, Random.Range(-screenBounds.y, screenBounds.y),Random.Range(-12,12));
+    }
+    IEnumerator asteroidWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnEnemy();
+        }
+    }
+}

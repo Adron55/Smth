a26432125cdd70f19e55ecc3823e771e113e3be9
fred <fredfrede414@gmail.com>
2019-10-19 00:57:13 +0400

    added roaster

--- Assets/EarthSimplePlanets/Scripts/ScriptsForSpawn/SpawnDebrisModels.cs
+++ Assets/EarthSimplePlanets/Scripts/ScriptsForSpawn/SpawnDebrisModels.cs
@@ -8,6 +8,7 @@ public class SpawnDebrisModels : MonoBehaviour
     public float respawnTime = 1.0f;
     private Vector2 screenBounds;
 
+    float x = 0.0f, y=0.0f, z=0.0f;
     // Use this for initialization
     void Start()
     {
@@ -17,7 +18,15 @@ public class SpawnDebrisModels : MonoBehaviour
     private void spawnEnemy()
     {
         GameObject a = Instantiate(asteroidPrefab) as GameObject;
-        a.transform.position = new Vector3(screenBounds.x * -2, Random.Range(-screenBounds.y, screenBounds.y),Random.Range(-12,12));
+        x=Random.Range(0f, 0.99f);
+        y= Random.Range(0.75f, 0.99f);
+        z= Random.Range(10.0f, 20.0f);
+        Vector3 v3Pos = Camera.main.ViewportToWorldPoint(new Vector3(x, y,z));
+        a.transform.localScale = new Vector3(.05f,.05f,.05f);
+        a.transform.SetPositionAndRotation(v3Pos, Random.rotationUniform);
+
+        Debug.Log(string.Format("x= {0} ; y= {1}", x, y));
+        //a.transform.position = v3Pos;//new Vector3(screenBounds.x * -2, Random.Range(-screenBounds.y, screenBounds.y),Random.Range(-12,12));
     }
     IEnumerator asteroidWave()
     {

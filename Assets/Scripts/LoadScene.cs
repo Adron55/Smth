2a04b55a6ef539a381c2bdc7416ad54e99bddcc1
Adron <anarmemmedli55@gmail.com>
2019-10-19 00:58:25 +0400

    Gui

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void LoadSceneManager(string value)
+    {
+        StartCoroutine(LoadYourAsyncScene(value));
+    }
+    private void Start()
+    {
+        StartCoroutine(LoadYourAsyncScene("SimplePlanetEarth"));
+    }
+    IEnumerator LoadYourAsyncScene(string value)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(value);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}

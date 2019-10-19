f9cb494d34c5091f6385a54f51efc88c181528d8
Adron <anarmemmedli55@gmail.com>
2019-10-19 12:47:44 +0400

    Gui

---
+++ Assets/Scripts/AudioStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioStart : MonoBehaviour
+{
+    AudioSource audioData;
+
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+        Debug.Log("started");
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
